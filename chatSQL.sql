f8a6680ee27b6399e20d5c599c40adc170801b81
Vishal Barot <vishalbarot@Vishals-MacBook-Pro.local>
2019-10-25 08:03:11 -0500

    database structure updated

--- chatSQL.sql
+++ chatSQL.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE ChatApp;
 
-CREATE TABLE User (
-    UserId int NOT NULL,
+CREATE TABLE `User`(
+    UserId int NOT NULL AUTO_INCREMENT,
     UserName varchar(50) NOT NULL,
-    UserEmail VARCHAR(50) NOT NULL
+    UserEmail VARCHAR(50) NOT NULL,
     Password varchar(50) NULL,
     CONSTRAINT USER_PK PRIMARY KEY (userId)
 )ENGINE=INNODB;
 
 CREATE TABLE `Message` (
-  `MessageId` int NOT NULL,
-  `MessageTime` timestamp  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `MessageId` int NOT NULL AUTO_INCREMENT,
+  `MessageTime` timestamp  NULL DEFAULT CURRENT_TIMESTAMP,
   `MessageText` varchar(255) NOT NULL,
    CONSTRAINT MESSAGE_PK PRIMARY KEY (MessageId)
 )ENGINE=INNODB;
