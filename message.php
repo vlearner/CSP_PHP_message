b4ae0cb54e2218f16086ecd52a09a8974edb429a
Vishal Barot <vishalbarot@Vishals-MacBook-Pro.local>
2019-10-29 07:06:53 -0500

    project updated

--- message.php
+++ message.php
@@ -15,15 +15,15 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
-
+     
 
         <?php 
+        include('dbconnect.php');
                 if(isset($_POST['sendMessage'])){
                     $messageText = $_POST['message'];
                     try
                     {
-                        include('dbconnect.php');
+                        // include('dbconnect.php');
                         $messageQuery = "INSERT INTO ChatApp.Message (MessageText)
                                     VALUES ('$messageText')";
 
@@ -38,12 +38,47 @@
             }
 
             //select query!!
-            $getMessage = "SELECT MessageTEXT FROM ChatApp.Message ORDER BY MessageId DESC";
+            // $getMessage = "SELECT MessageTEXT FROM ChatApp.Message ORDER BY MessageId DESC";
+            // $statement = $dbConnect->prepare($getMessage);
+            // $statement->execute();
+            // $articles = $statement->fetchAll();
+            // // $statement->closeCursor();
+            // foreach(new tableRows (new RecursiveArrayIterator($statement->fetchAll()))
+            //         as $k=>$v){
+            //             echo $v;
+            //         }
+
+            // $conn =$dbConnect;
+            // $getMessage = "SELECT MessageTEXT FROM ChatApp.Message ORDER BY MessageId DESC";
+            // $rs=mysqli_query($conn, $getMessage);
+            // $count = mysqli_num_rows($rs);
+            // if($count > 0){
+            //     while($row){
+            //         while($row = mysqli_fetch_array($rs)){
+            //             echo $row['message'] . "<br>";
+            //         }
+            //     }
+            // }
             
 
+            // $result = $dbConnect->prepare("SELECT MessageTEXT FROM ChatApp.Message ORDER BY MessageId DESC"); 
+            // $result->execute();
+            // $row_count =var_dump($result->fetchAll());
+            // echo var_dump($row_count);
         
+
+                $getMessage = "SELECT * FROM ChatApp.Message ORDER BY MessageId DESC";
+                $chat = $dbConnect->prepare($getMessage);
+                $chat->execute();
+             // $chat = $dbConnect->execute($getMessage);
+                $chatInfo = $chat->fetchAll();
+                // $chat = $chat->fetchALL();
         
         ?>
+
+
+           
+           
           <script src="" async defer>
             function sendmessage(){
                 var message =  formMessage.message.value;
@@ -72,12 +107,22 @@
             </form>
         </div>
         <h4>Note</h4>
-        <h5>Change database structure</h5>
-
-
-        <div>
-            <input type="area" name=class="showMessage" disabled placeholder="Message Loading...">
-        </div>
+    
+            <tbody>
+        <?php
+        if(!empty($chatInfo)){
+            foreach ($chatInfo as $chatDetail) { ?>
+            <tr class="table-row">
+                <td>
+                       <?php echo $chatDetail['MessageText']; ?> <br>
+                </td>
+            </tr>
+                   
+            <?php } 
+                } 
+                ?>
+        </tbody>
+    
         
         <script src="" async defer>
             function sendmessage(){
